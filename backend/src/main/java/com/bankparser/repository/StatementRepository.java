26dc0131143b3ca1cdd99ca814f7ed2ee243e7c3
hasnain <mdhasnain85629@gmil.com>
2026-03-25 01:51:10 +0530

    added transactionRepository.java and StatementRepository.java

---
+++ backend/src/main/java/com/bankparser/repository/StatementRepository.java
@@ -0,0 +1,11 @@
+package com.bankparser.repository;
+
+import com.bankparser.model.Statement;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface StatementRepository extends JpaRepository<Statement, Long> {
+    List<Statement> findAllByOrderByCreatedAtDesc();
+}
